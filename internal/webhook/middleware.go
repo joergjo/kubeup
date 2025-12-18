13a7ce867087aab44f617cb042d7a8c795031049
Jörg Jooss <joerg@joergjooss.de>
2025-12-18 15:41:18 +0100

    refactor: align parameter names
    Signed-off-by: Jörg Jooss <joerg@joergjooss.de>

--- internal/webhook/middleware.go
+++ internal/webhook/middleware.go
@@ -20,13 +20,13 @@ const accessTokenParam = "access_token"
 
 // newClientSecretMiddleware creates a middleware that validates the client secret in request query parameters.
 // It expects the secret in the "access_token" query parameter and validates it against two possible values (for rotation).
-func newClientSecretMiddleware(key1, key2 string) func(http.Handler) http.Handler {
-	key1B := []byte(key1)
-	key2B := []byte(key2)
+func newClientSecretMiddleware(sec1, sec2 string) func(http.Handler) http.Handler {
+	sec1B := []byte(sec1)
+	sec2B := []byte(sec2)
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			tokenB := []byte(r.URL.Query().Get(accessTokenParam))
-			if subtle.ConstantTimeCompare(key1B, tokenB) != 1 && subtle.ConstantTimeCompare(key2B, tokenB) != 1 {
+			if subtle.ConstantTimeCompare(sec1B, tokenB) != 1 && subtle.ConstantTimeCompare(sec2B, tokenB) != 1 {
 				slog.Warn("received request with invalid secret")
 				http.Error(w, "invalid secret", http.StatusUnauthorized)
 				return
