4c950ce8e8bd57cda4a1bc1df1957bf72f205336
Jörg Jooss <joerg@joergjooss.de>
2026-04-08 22:38:01 +0200

    fix: embed only *.gohtml
    Signed-off-by: Jörg Jooss <joerg@joergjooss.de>

--- internal/templates/fs.go
+++ internal/templates/fs.go
@@ -5,5 +5,5 @@ import "embed"
 // FS is an embedded filesystem containing all email templates.
 // It includes HTML templates for the various AKS event types.
 //
-//go:embed *
+//go:embed *.gohtml
 var FS embed.FS
